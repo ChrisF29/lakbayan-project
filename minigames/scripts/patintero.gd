f76c092bb8bd21b484344cf0946bf108a143a398
Chris Ferrer <imjunjun24@gmail.com>
2026-06-05 18:21:33 +0800

    Refactor code structure for improved readability and maintainability

--- minigames/scripts/patintero.gd
+++ minigames/scripts/patintero.gd
@@ -1,32 +1,9 @@
-extends Control
-
-@onready var title = $Title
-@onready var hint = $Hint
+extends Node2D
 
 func _ready():
 
-	if title:
-		title.text = "PATINTERO"
-
-	if hint:
-		hint.text = "Mini game placeholder"
+pass
 
 func _on_back_pressed() -> void:
 
-	QuestManager.set_state(
-		"anito_quest_complete",
-		true
-	)
-
-	QuestManager.set_state(
-		"babaylan_help_done",
-		true
-	)
-
-	QuestManager.complete_quest(
-		"Kunin ang Sagradong anito."
-	)
-
-	get_tree().change_scene_to_file(
-		"res://maps/pre_colonial_main_map.tscn"
-	)
+pass
